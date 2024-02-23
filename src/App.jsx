51b34886cd14835f7c7b9fee791818ece95bfb2e
Tech-Kat <katpelle@pursuit.org>
2024-02-23 11:26:56 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Router, Routes } from "react-router-dom"
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      {/* <Router>
+        <main>
+          <Routes>
+            <Route path="/"/>
+          </Routes>
+        </main>
+      </Router> */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2>"Empowering Creativity, Connecting Hearts: Our mission at Kendra's Art Gallery is to curate and showcase a diverse collection of exceptional paintings from talented artists worldwide. By providing a seamless platform for art enthusiasts to discover, appreciate, and acquire unique pieces, we aim to foster a global community that celebrates creativity and promotes the transformative power of art in enriching lives."</h2>
+        </div>
+    )
+}
